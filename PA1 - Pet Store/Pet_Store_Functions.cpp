864093d9c40b3350ebf8e86f6d673451a966bee9
Gabe DiMartino <gdimartino@gabedimartino.com>
2023-08-31 20:22:20 -0700

    Started Functions

--- PA1 - Pet Store/Pet_Store_Functions.cpp
+++ PA1 - Pet Store/Pet_Store_Functions.cpp
@@ -5,7 +5,56 @@
  * Description:     Functions for the Pet Store Main
  */
 
-#include<iostream>
-#include<fstream>
-#include<sstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+bool fileOpen(std::string, std::ifstream&);
+std::string printFile(std::ifstream&);
+void fileClose(std::ifstream&);
+
+int main(){
+    std::ifstream myFile;
+    std::string filename = "petstoredata.csv";
+
+    bool isOpen;
+    std::string text;
+
+    isOpen = fileOpen(filename, myFile);
+
+    if(isOpen){ 
+        text = printFile(myFile);
+        std::cout << text <<std::endl;
+        fileClose(myFile);
+    }
+    else 
+        std::cout << "The file " + filename + " has encountered an error. Please make sure the file is in the correct directory and you have the appropriate permissions to read/write/execute" << std::endl;
+    return 0;
+}
+
+
+
+bool fileOpen(std::string filename, std::ifstream& myFile){
+    myFile.open(filename);
+    if(myFile.is_open())
+        return true;
+    else
+        return false;
+
+}
+
+std::string printFile(std::ifstream& myFile){
+    std::string line;
+    std::string text;
+
+    while (std::getline(myFile, line)){
+        text += line + "\n";
+    }
+    return text;
+}
+
+void fileClose(std::ifstream& myFile){
+    myFile.close();
+}
+    
 
